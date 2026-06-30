8dec1bda033dd9613a020884474ffaba098bd7c8
Wangai <wangai.c.w@gmail.com>
2026-06-30 23:11:52 +0300

    Added: play/pause toggle button based on audio state

--- src/AudioPlayer.jsx
+++ src/AudioPlayer.jsx
@@ -49,6 +49,19 @@ export default function NowPlaying({ title, artist, album, artUrl, isPlaying = f
                 </p>
             </div>
 
+            {/* Play/Pause Icon */}
+            <div style={{ flexShrink: 0, display: "flex", alignItems: "center", justifyContent: "center", width: 32, height: 32, borderRadius: "50%", background: "rgba(255, 255, 255, 0.2)", transition: "background 0.3s ease" }}>
+                {isPlaying ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style={{width: 16, height: 16, color: "white"}}>
+                      <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z" clipRule="evenodd" />
+                    </svg>
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style={{width: 16, height: 16, color: "white", marginLeft: 2}}>
+                      <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
+                    </svg>
+                )}
+            </div>
+
             {/* Animated bars */}
             <div style={{ display: "flex", gap: 2, alignItems: "center", height: 20, flexShrink: 0 }}>
                 {bars.map((delay, i) => (
